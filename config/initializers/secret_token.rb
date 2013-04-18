e4b3875fdc17160941af85df5767f02e5f0f439a
Firas Steitiyeh <fsteitiyeh@gmail.com>
2013-04-18 23:30:26 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fiveaside::Application.config.secret_token = 'cd421ab76e06a39f67a6c2ca599a8fbb2300d0c3c2c5dd024ad8976e140a41a4593330320dfe4911d220b2e17f383a3dc03834d566b4cea56687041e6fa19df3'
